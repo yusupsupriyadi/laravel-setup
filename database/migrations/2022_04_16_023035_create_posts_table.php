f3af61c22eb00d38e3659957ab52c8d2aae81c47
yusupsupriyadi <yusupsupriyadistudio@gmail.com>
2022-04-16 09:39:13 +0700

    update modal and migration

--- database/migrations/2022_04_16_023035_create_posts_table.php
+++ database/migrations/2022_04_16_023035_create_posts_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('body');
             $table->timestamps();
         });
     }
